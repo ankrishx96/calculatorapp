ff816a057dde662b5f6d49e7123b5cdf1f690f68
Ankrishx20 <ankrishkandpal1996@gmail.com>
2019-06-26 21:57:32 +0530

    Updates

---
+++ precache-manifest.1b41b5025dbf79099dbac61850952bb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6a33799032a7a5400b7f778835fb0e",
+    "url": "/calculatorapp/index.html"
+  },
+  {
+    "revision": "13f4223fa6ad7507fe60",
+    "url": "/calculatorapp/static/css/main.96c46737.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/calculatorapp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "13f4223fa6ad7507fe60",
+    "url": "/calculatorapp/static/js/main.b63687aa.chunk.js"
+  },
+  {
+    "revision": "b97279c050665847210c",
+    "url": "/calculatorapp/static/js/runtime~main.06e850aa.js"
+  }
+]);
